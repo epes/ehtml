891a532ff5706969ccdecaa83063e03f3fc608e5
Robert Eperjesi <epesdev@gmail.com>
2020-12-13 00:01:05 -0600

    Find nested

--- find.go
+++ find.go
@@ -30,11 +30,29 @@ func Find(root *html.Node, fn FindTestFn) []*html.Node {
 	return res
 }
 
-// FindClass is sugar for Find for a specific class attribute.
-func FindClass(root *html.Node, class string) []*html.Node {
+// FindNested applies multiple testing functions in sequence to find
+// the nodes which satisfy all of them.
+func FindNested(root *html.Node, fns []FindTestFn) []*html.Node {
+	q := newNodeQueue()
+	q.Enqueue(root)
+
+	for _, fn := range fns {
+		newQ := newNodeQueue()
+
+		for node := q.Dequeue(); node != nil; node = q.Dequeue() {
+			newQ.EnqueueSlice(Find(node, fn))
+		}
+
+		q = newQ
+	}
+
+	return q.Slice()
+}
+
+func getClassTestingFn(class string) FindTestFn {
 	class = strings.ToLower(class)
 
-	return Find(root, func(n *html.Node) bool {
+	return func(n *html.Node) bool {
 		if n.Type == html.ElementNode {
 			for _, a := range n.Attr {
 				if a.Key == "class" && strings.ToLower(a.Val) == class {
@@ -44,14 +62,31 @@ func FindClass(root *html.Node, class string) []*html.Node {
 		}
 
 		return false
-	})
+	}
 }
 
-// FindTags is sugar for Find for a specific element name.
-func FindTags(root *html.Node, tag string) []*html.Node {
+// FindClass is sugar for Find for a specific class attribute.
+func FindClass(root *html.Node, class string) []*html.Node {
+	class = strings.ToLower(class)
+
+	return Find(root, getClassTestingFn(class))
+}
+
+// FindClasses finds the elements which match the specified nested classes.
+func FindClasses(root *html.Node, classes []string) []*html.Node {
+	var fns []FindTestFn
+
+	for _, c := range classes {
+		fns = append(fns, getClassTestingFn(c))
+	}
+
+	return FindNested(root, fns)
+}
+
+func getTagTestingFn(tag string) FindTestFn {
 	tag = strings.ToLower(tag)
 
-	return Find(root, func(n *html.Node) bool {
+	return func(n *html.Node) bool {
 		fmt.Println(n.Data)
 
 		if n.Type == html.ElementNode && strings.ToLower(n.Data) == tag {
@@ -59,7 +94,24 @@ func FindTags(root *html.Node, tag string) []*html.Node {
 		}
 
 		return false
-	})
+	}
+}
+
+// FindTag is sugar for Find for a specific element name.
+func FindTag(root *html.Node, tag string) []*html.Node {
+
+	return Find(root, getTagTestingFn(tag))
+}
+
+// FindTags finds the elements which match the specified nested tags.
+func FindTags(root *html.Node, tags []string) []*html.Node {
+	var fns []FindTestFn
+
+	for _, t := range tags {
+		fns = append(fns, getTagTestingFn(t))
+	}
+
+	return FindNested(root, fns)
 }
 
 // FindText finds all html.TextNode within the specified root tree.
--- queue.go
+++ queue.go
@@ -19,43 +19,61 @@ func newNodeQueue() *queue {
 }
 
 // Take the next item off the front of the queue
-func (this *queue) Dequeue() *html.Node {
-	if this.length == 0 {
+func (q *queue) Dequeue() *html.Node {
+	if q.length == 0 {
 		return nil
 	}
-	n := this.start
-	if this.length == 1 {
-		this.start = nil
-		this.end = nil
+	n := q.start
+	if q.length == 1 {
+		q.start = nil
+		q.end = nil
 	} else {
-		this.start = this.start.next
+		q.start = q.start.next
 	}
-	this.length--
+	q.length--
 	return n.value
 }
 
 // Put an item on the end of a queue
-func (this *queue) Enqueue(value *html.Node) {
+func (q *queue) Enqueue(value *html.Node) {
 	n := &node{value, nil}
-	if this.length == 0 {
-		this.start = n
-		this.end = n
+	if q.length == 0 {
+		q.start = n
+		q.end = n
 	} else {
-		this.end.next = n
-		this.end = n
+		q.end.next = n
+		q.end = n
 	}
-	this.length++
+	q.length++
+}
+
+// EnqueueSlice takes a whole slice of nodes and enqueues them.
+func (q *queue) EnqueueSlice(values []*html.Node) {
+	for _, v := range values {
+		q.Enqueue(v)
+	}
 }
 
 // Return the number of items in the queue
-func (this *queue) Len() int {
-	return this.length
+func (q *queue) Len() int {
+	return q.length
 }
 
 // Return the first item in the queue without removing it
-func (this *queue) Peek() *html.Node {
-	if this.length == 0 {
+func (q *queue) Peek() *html.Node {
+	if q.length == 0 {
 		return nil
 	}
-	return this.start.value
+	return q.start.value
+}
+
+// Slice returns the queue as a slice of nodes.
+func (q *queue) Slice() []*html.Node {
+	s := make([]*html.Node, q.Len())
+
+	for i := 0; i < q.Len(); i++ {
+		s[i] = q.Dequeue()
+	}
+
+	return s
 }
