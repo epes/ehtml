585dcd0b7a68b5bd769c58dc2fb02ca4e183412a
Robert Eperjesi <epesdev@gmail.com>
2020-12-15 00:00:37 -0600

    Only FindJ now. +bugfixes +tests

--- find.go
+++ find.go
@@ -13,10 +13,18 @@ type FindTestFn func(*html.Node) bool
 // Find applies the tester function to all nodes within the specified
 // root subtree and returns all nodes that pass.
 func Find(root *html.Node, fn FindTestFn) []*html.Node {
+	if root == nil {
+		return nil
+	}
+
 	q := newNodeQueue()
 	var res []*html.Node
 
-	for node := root; node != nil; node = q.Dequeue() {
+	for c := root.FirstChild; c != nil; c = c.NextSibling {
+		q.Enqueue(c)
+	}
+
+	for node := q.Dequeue(); node != nil; node = q.Dequeue() {
 		if fn(node) {
 			res = append(res, node)
 		}
@@ -29,9 +37,7 @@ func Find(root *html.Node, fn FindTestFn) []*html.Node {
 	return res
 }
 
-// FindNested applies multiple testing functions in sequence to find
-// the nodes which satisfy all of them.
-func FindNested(root *html.Node, fns []FindTestFn) []*html.Node {
+func findNested(root *html.Node, fns []FindTestFn) []*html.Node {
 	q := newNodeQueue()
 
 	if root != nil {
@@ -57,6 +63,10 @@ func FindNested(root *html.Node, fns []FindTestFn) []*html.Node {
 
 // FindJ finds nested nodes given selectors similar to JavaScript querySelectors.
 func FindJ(root *html.Node, joinedSelectors string) []*html.Node {
+	if root == nil || joinedSelectors == "" {
+		return nil
+	}
+
 	var fns []FindTestFn
 
 	selectors := strings.Split(joinedSelectors, " ")
@@ -76,7 +86,7 @@ func FindJ(root *html.Node, joinedSelectors string) []*html.Node {
 		}
 	}
 
-	return FindNested(root, fns)
+	return findNested(root, fns)
 }
 
 func getClassTestingFn(class string) FindTestFn {
@@ -85,8 +95,13 @@ func getClassTestingFn(class string) FindTestFn {
 	return func(n *html.Node) bool {
 		if n.Type == html.ElementNode {
 			for _, a := range n.Attr {
-				if a.Key == "class" && strings.ToLower(a.Val) == class {
-					return true
+				if a.Key == "class" {
+					classes := strings.Split(a.Val, " ")
+					for _, c := range classes {
+						if strings.ToLower(c) == class {
+							return true
+						}
+					}
 				}
 			}
 		}
@@ -95,61 +110,14 @@ func getClassTestingFn(class string) FindTestFn {
 	}
 }
 
-// FindClass is sugar for Find for a specific class attribute.
-func FindClass(root *html.Node, class string) []*html.Node {
-	class = strings.ToLower(class)
-
-	return Find(root, getClassTestingFn(class))
-}
-
-// FindClasses finds the elements which match the specified nested classes.
-func FindClasses(root *html.Node, classes []string) []*html.Node {
-	var fns []FindTestFn
-
-	for _, c := range classes {
-		fns = append(fns, getClassTestingFn(c))
-	}
-
-	return FindNested(root, fns)
-}
-
 func getTagTestingFn(tag string) FindTestFn {
 	tag = strings.ToLower(tag)
 
 	return func(n *html.Node) bool {
-		if n.Type == html.ElementNode && strings.ToLower(n.Data) == tag {
-			return true
-		}
-
-		return false
+		return n.Type == html.ElementNode && strings.ToLower(n.Data) == tag
 	}
-}
-
-// FindTag is sugar for Find for a specific element name.
-func FindTag(root *html.Node, tag string) []*html.Node {
-	return Find(root, getTagTestingFn(tag))
-}
-
-// FindTags finds the elements which match the specified nested tags.
-func FindTags(root *html.Node, tags []string) []*html.Node {
-	var fns []FindTestFn
-
-	for _, t := range tags {
-		fns = append(fns, getTagTestingFn(t))
-	}
-
-	return FindNested(root, fns)
 }
 
 func textTestingFn(n *html.Node) bool {
-	if n.Type == html.TextNode {
-		return true
-	}
-
-	return false
-}
-
-// FindText finds all html.TextNode within the specified root tree.
-func FindText(root *html.Node) []*html.Node {
-	return Find(root, textTestingFn)
+	return n.Type == html.TextNode
 }
--- find_test.go
+++ find_test.go
@@ -1,11 +1,56 @@
 package ehtml_test
 
 import (
+	"fmt"
+	"strings"
 	"testing"
 
 	"github.com/epes/ehtml"
+	"golang.org/x/net/html"
 )
 
 func TestFindJ(t *testing.T) {
-	ehtml.FindJ(nil, ".hello a .world")
+	tests := []struct {
+		in         string
+		expression string
+		len        int
+	}{
+		{"", "", 0},
+		{"", ".hello", 0},
+		{"", ".hello a *", 0},
+
+		{"<div>foobar</div>", "div", 1},
+		{"<div>foobar</div>", "div *", 1},
+		{"<div>foobar</div>", "div div", 0},
+		{"<div>foobar</div>", "*", 1},
+
+		{`<div class="one two"><script>alert(1)</script><script>alert(2)</script></div>`, "", 0},
+		{`<div class="one two"><script>alert(1)</script><script>alert(2)</script></div>`, "*", 2},
+		{`<div class="one two"><script>alert(1)</script><script>alert(2)</script></div>`, "div script", 2},
+		{`<div class="one two"><script>alert(1)</script><script>alert(2)</script></div>`, "div script *", 2},
+		{`<div class="one two"><script>alert(1)</script><script>alert(2)</script></div>`, ".one script *", 2},
+
+		{`<div class="one"><div class="two three">foo<div class="four">bar</div></div></div>`, "", 0},
+		{`<div class="one"><div class="two three">foo<div class="four">bar</div></div></div>`, ".one .three .four", 1},
+		{`<div class="one"><div class="two three">foo<div class="four">bar</div></div></div>`, "*", 2},
+		{`<div class="one"><div class="two three">foo<div class="four">bar</div></div></div>`, ".one .three .four *", 1},
+		{`<div class="one"><div class="two three">foo<div class="four">bar</div></div></div>`, ".two *", 2},
+		{`<div class="one"><div class="two three">foo<div class="four">bar</div></div></div>`, ".one div", 2},
+	}
+
+	for _, tt := range tests {
+		doc, err := html.Parse(strings.NewReader(tt.in))
+		if err != nil {
+			t.Error(tt.len, tt.in, tt.expression, err)
+		}
+
+		found := ehtml.FindJ(doc, tt.expression)
+		if len(found) != tt.len {
+			t.Error(
+				fmt.Sprintf("got:%d want:%d", len(found), tt.len),
+				tt.in,
+				tt.expression,
+			)
+		}
+	}
 }
